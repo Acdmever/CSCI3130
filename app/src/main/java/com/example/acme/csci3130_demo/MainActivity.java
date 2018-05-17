404a5550e8ada43ed9d8b0f1036ad94d29d1bf86
Acdmever <36144334+Acdmever@users.noreply.github.com>
2018-05-16 21:06:22 -0300

    Update MainActivity.java

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -4,7 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    
+    public void update(View v) {
+        TextView text = (TextView) findViewById(R.id.textView);
+        EditText text2 = (EditText) findViewById(R.id.editText);
+        text.setText(text2.getText().toString());
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
